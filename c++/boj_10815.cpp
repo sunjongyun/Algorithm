0a961a41e4c7d00e6949e8ca52aeb1d42cea9677
first commit <>
2018-09-28 18:27:11 +0900

    set을 이용한 풀이 , 10만 이상의 데이터는 scanf 사용하고 [cin은 No]
    문제 : 숫자카드
    https://www.acmicpc.net/problem/10815

---
+++ c++/boj_10815.cpp
@@ -0,0 +1,30 @@
+//
+// Created by mac on 2018. 9. 28..
+//
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    set<int> card;
+
+    while (n--) {
+        int x;
+        scanf("%d", &x);
+        card.insert(x);
+    }
+
+    int m;
+    cin >> m;
+    while (m--) {
+        int x;
+        scanf("%d", &x);
+        cout << card.count(x) << ' ';
+    }
+
+    return 0;
+}
