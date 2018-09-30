f44b263839a6482789bb64efd80627185bef5975
first commit <>
2018-09-30 15:39:05 +0900

    string을 이용한 문제 ( eof 만날때까지 돌기 )
    문제 : 단어의 개수
    https://www.acmicpc.net/problem/1152

---
+++ c++/boj_1152.cpp
@@ -0,0 +1,22 @@
+//
+// Created by mac on 2018. 9. 30..
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string s;
+    int ans = 0;
+
+    while (cin >> s) {
+        ans++;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
