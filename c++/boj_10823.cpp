b3e0fd3d9e54c811b2a697f6de15ae180b13256a
first commit <>
2018-09-30 17:14:12 +0900

    istringstream을 이용한 문제 ( cin이 아닌 getline에서 문자열 자르기 )
    문제 : 더하기 2
    https://www.acmicpc.net/problem/10823

---
+++ c++/boj_10823.cpp
@@ -0,0 +1,30 @@
+//
+// Created by mac on 2018. 9. 30..
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    string s = "";
+    string seg = "";
+    int ans = 0;
+
+    while (getline(cin, seg)) {
+        s += seg;
+    }
+
+    istringstream sin(s);
+
+    while (getline(sin, s, ',')) {
+        ans += stoi(s);
+    }
+
+    cout << ans << '\n';
+
+
+    return 0;
+}
