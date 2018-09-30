b2203c2ec58e6fd1f371a1b7a1e1210c29f19626
first commit <>
2018-09-30 17:02:18 +0900

    stoi을 이용한 문제 ( getline에서 문자열 자르기 )
    문제 : 더하기
    https://www.acmicpc.net/problem/10822

---
+++ c++/boj_10822.cpp
@@ -0,0 +1,21 @@
+//
+// Created by mac on 2018. 9. 30..
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    int ans = 0;
+
+    while (getline(cin, s, ',')) {
+        ans += stoi(s);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
