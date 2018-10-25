e261e5986e7d138ef75f22976598c043e7c7fa8c
first commit <>
2018-10-25 16:08:16 +0900

    .

---
+++ c++/boj_13460.cpp
@@ -0,0 +1,35 @@
+//
+// Created by mac on 2018. 10. 20..
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int map[n][m];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> map[i][j];
+        }
+    }
+
+    int life=10; // 기울이는 횟수는 최대 10번
+
+    while (life--){
+
+
+
+
+
+    }
+
+
+    return 0;
+}
---
+++ c++/boj_2011.cpp
@@ -0,0 +1,40 @@
+//
+// Created by mac on 2018. 10. 23..
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+long long d[5001];
+int mod = 1000000;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    s = " " + s;
+
+    d[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        int x = s[i] - '0';
+        if (1 <= x && x <= 9) {
+            d[i] += d[i - 1];
+            d[i] %= mod;
+        }
+
+        if (i == 1) continue;
+
+        x = (s[i - 1] - '0') * 10 + (s[i] - '0');
+        if (10 <= x && x <= 26) {
+            d[i] += d[i - 2];
+            d[i] %= mod;
+        }
+    }
+
+    cout << d[n] << '\n';
+
+    return 0;
+}
