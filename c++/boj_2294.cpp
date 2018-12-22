16e1b7e217657069ca66c2d2e02d1e51e561750d
SUN JONG-YEON <sunjongyun@naver.com>
2018-12-23 01:42:23 +0900

    문제 : 동전2 ( https://www.acmicpc.net/problem/2294 ) 분류 : dp

--- c++/boj_2294.cpp
+++ c++/boj_2294.cpp
@@ -11,8 +11,8 @@ using namespace std;
 const int INF = 987654321;
 int dp[10001];
 
-int solve(int n, int k, vector<int> coins) {
-    if (k < 0) {
+int solve(int n, int k, vector<int> coins) { // search k Won
+    if (k < 0) { // There is no value less than k.
         return INF;
     }
 
@@ -20,13 +20,16 @@ int solve(int n, int k, vector<int> coins) {
         return dp[0];
     }
 
-    if (dp[k] != -1) return dp[k];
+    if (dp[k] != -1) return dp[k]; // memoization
 
     dp[k] = INF;
+
+    /* ex: 1원, 3원, 5원을 가지고 문제를 풀면,
+     * dp[n] = min( dp[n-1]+1, dp[n-3]+1, dp[n-5]+1) 을 구하는 과정
+     * */
     for (int i = 0; i < n; i++) {
         dp[k] = min(dp[k], solve(n, k - coins[i], coins) + 1);
     }
-
 
     return dp[k];
 }
@@ -42,7 +45,7 @@ int main() {
         cin >> coins[i];
     }
 
-    memset(dp, -1, sizeof(dp)); // initialize -1
+    memset(dp, -1, sizeof(dp)); // initialize -1 .. -1 is not visited, another value is visited
     dp[0] = 0;
     int ans = solve(n, k, coins);
 
