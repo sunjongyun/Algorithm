2ef3f679de25721ab2d85fc414811074d2deaebc
SUN JONG-YEON <sunjongyun@naver.com>
2018-12-23 01:35:48 +0900

    문제 : 동전2 ( https://www.acmicpc.net/problem/2294 ) 분류 : dp

--- c++/boj_2294.cpp
+++ c++/boj_2294.cpp
@@ -2,3 +2,55 @@
 // Created by mac on 2018. 10. 25..
 //
 
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+const int INF = 987654321;
+int dp[10001];
+
+int solve(int n, int k, vector<int> coins) {
+    if (k < 0) {
+        return INF;
+    }
+
+    if (k == 0) {
+        return dp[0];
+    }
+
+    if (dp[k] != -1) return dp[k];
+
+    dp[k] = INF;
+    for (int i = 0; i < n; i++) {
+        dp[k] = min(dp[k], solve(n, k - coins[i], coins) + 1);
+    }
+
+
+    return dp[k];
+}
+
+int main() {
+    int n, k;
+
+    cin >> n >> k;
+
+    vector<int> coins(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    memset(dp, -1, sizeof(dp)); // initialize -1
+    dp[0] = 0;
+    int ans = solve(n, k, coins);
+
+    if (ans == INF) {
+        cout << -1 << '\n';
+    } else {
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
