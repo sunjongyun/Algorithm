d46f0fb4c2d31a5aaeccc8b8d5efe6db50111acf
SUN JONG-YEON <sunjongyun@naver.com>
2018-12-31 03:08:37 +0900

    문제 : 문자열을 정수로 바꾸기 ( https://programmers.co.kr/learn/courses/30/lessons/12925 ) 분류 :

---
+++ c++/kakao_Replace string with integer.cpp
@@ -0,0 +1,18 @@
+//
+// Created by SUN on 2018-12-31.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
+
+int main() {
+    cout << solution("-1234") << '\n';
+
+    return 0;
+}
